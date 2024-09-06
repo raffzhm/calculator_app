9402329dc8953fe8d871248478bc813c11d0f1f5
raffzhm <rafiazhim.ra@gmail.com>
2024-09-06 15:31:06 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('Kalkulator')),
         body: Kalkulator(),
