137dc1d5ac21c94c88220b3722db7f8cd591dcf0
raffzhm <rafiazhim.ra@gmail.com>
2024-09-06 15:16:19 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(KalkulatorApp());
+
+class KalkulatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Kalkulator')),
+        body: Kalkulator(),
+      ),
+    );
+  }
+}
+
+class Kalkulator extends StatefulWidget {
+  @override
+  _KalkulatorState createState() => _KalkulatorState();
+}
+
+class _KalkulatorState extends State<Kalkulator> {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0;
+  double num2 = 0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "×" ||
+        buttonText == "÷") {
+      num1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      } else if (operand == "-") {
+        _output = (num1 - num2).toString();
+      } else if (operand == "×") {
+        _output = (num1 * num2).toString();
+      } else if (operand == "÷") {
+        _output = (num1 / num2).toString();
+      }
+      num1 = 0;
+      num2 = 0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+          child: Text(
+            output,
+            style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Expanded(
+          child: Divider(),
+        ),
+        Column(
+          children: [
+            Row(
+              children: <Widget>[
+                buildButton("7"),
+                buildButton("8"),
+                buildButton("9"),
+                buildButton("÷")
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                buildButton("4"),
+                buildButton("5"),
+                buildButton("6"),
+                buildButton("×")
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                buildButton("1"),
+                buildButton("2"),
+                buildButton("3"),
+                buildButton("-")
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                buildButton("."),
+                buildButton("0"),
+                buildButton("00"),
+                buildButton("+")
+              ],
+            ),
+            Row(
+              children: <Widget>[buildButton("CLEAR"), buildButton("=")],
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
